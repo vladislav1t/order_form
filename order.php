7c6e4a44316f1726c2ffe470392134b16b9a1cbb
vtimiryanov <76KsYW&rqXa3eWQC>
2021-04-20 09:10:17 +0300

    session

--- order.php
+++ order.php
@@ -1,36 +1,24 @@
 <?php
 session_start();
 require_once __DIR__ . '/items.php';
+require_once __DIR__ . '/functions.php';
 
-function validate_russian_phone_number($tel)
-{
-    $tel = trim((string)$tel);
-    $tel = preg_replace('#[^0-9+]+#uis', '', $tel);
-    if (!preg_match('#^(?:\\+?7|8|)(.*?)$#uis', $tel, $m)) return false;
-    $tel = '+7' . preg_replace('#[^0-9]+#uis', '', $m[1]);
-    if (!preg_match('#^\\+7[0-9]{10}$#uis', $tel, $m)) return false;
-    return $tel;
-}
 
 // @todo: сделать проверки
 // @todo: добавить дату заказа
 $data = [
     'userInfo' => [
         $_POST['name'],
-        $_POST['email2'],
+        $_POST['email'],
         $_POST['phone'],
         date('Y-m-d H:i')
     ]
 ];
 $userInfo = [
     $_POST['name'],
-    $_POST['email2'],
+    $_POST['email'],
     $_POST['phone'],
 ];
-
-$_SESSION['name'] = $_POST['name'] ?? '';
-$_SESSION['email'] = $_POST['email2'] ?? '';
-$_SESSION['phone'] = $_POST['phone'] ?? '';
 
 $productOrders = !empty($_POST['quantity']) && is_array($_POST['quantity']) ?
     array_filter(
@@ -59,27 +47,28 @@ if (count($productOrders)) {
     if(empty($_POST['name'])) {
         $_SESSION['errors']['name'] = 'Заполните Имя!';
         header('Location: / ');
-        exit();
     }
 
     if(empty($_POST['email'])) {
         $_SESSION['errors']['email'] = 'Заполните Email!';
-        header('Location: / ');exit();
+        header('Location: / ');
     } else {
         if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
             $_SESSION['errors']['email'] = 'Email заполнен не правильно!';
-            header('Location: / ');exit();
+            header('Location: / ');
         }
     }
 
     if (empty($_POST['phone'])) {
         $_SESSION['errors']['phone'] = 'Заполните Телефон!';
         header('Location: / ');
-        exit();
     }
     elseif (validate_russian_phone_number($_POST['phone'])) {
         $_SESSION['errors']['phone'] = 'Телефон заполнен не правильно!';
         header('Location: / ');
+    }
+
+    if (!empty($_SESSION['errors'])) {
         exit();
     }
 
